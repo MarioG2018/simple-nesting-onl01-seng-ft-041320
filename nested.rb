423bf0960b5e659d0d40b825d67efda8a175334e
Mario Garza <mario.garza02@utrgv.edu>
2020-04-22 01:46:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,6 +133,6 @@ def adding_to_dennis
           #added Assembly
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+     programmer_hash[:dennis_ritchie][:languages].push "Assembly"
      programmer_hash
 end
