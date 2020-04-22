dc6edd9f5fd1aec0bb50323bb3e09ab2b1ef12d2
Mario Garza <mario.garza02@utrgv.edu>
2020-04-22 01:43:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]="C"
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -68,10 +68,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		    :yukihiro_matsumoto => {
- 		      :known_for =>"Ruby",
- 		      :languages =>["LISP","C"]
- 		    },
+# 		    :yukihiro_matsumoto => {
+# 		      :known_for =>"Ruby",
+# 		      :languages =>["LISP","C"]
+# 		    },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -85,12 +85,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash=yukihiro_matsumoto
-     programmer_hash[:yukihiro_matsumoto]= :known_for
-     programmer_hash[:yukihiro_matsumoto]= :languages
+     programmer_hash[:yukihiro_matsumoto]={}
      programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
-     programmer_hash[:yukihiro_matsumoto][:languages]="LISP","C"
-  programmer_hash
+     programmer_hash[:yukihiro_matsumoto][:languages]=["LISP","C"]
+     programmer_hash
 end
 
 def changing_alan
@@ -101,7 +99,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "GUI",
+          :known_for => "Object Orientation",
           #changed Object Orientation to GUI
           :languages => ["Smalltalk", "LISP"]
         },
@@ -111,8 +109,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the #alans_new_info variable. 
-     #alans_new_info = "GUI"
-     #programmer_hash[:alan_kay][:known_for]=alans_new_info
+     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for]= alans_new_info
+     programmer_hash
      
      
 end
@@ -130,10 +129,11 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C","Assembly"]
+          :languages => ["C"]
           #added Assembly
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]="C,""Assembly"
      programmer_hash
 
 end
